8cf3aa46c27d162034bf2faed93dc90e03c6e4fa
Sito8943 <sito8943@gmail.com>
2022-07-23 13:18:32 -0700

    remove console.log

--- src/views/Menu/Edit.jsx
+++ src/views/Menu/Edit.jsx
@@ -282,7 +282,6 @@ const Edit = () => {
       ph: photo,
     };
     const indexes = [];
-    console.log(parsedData);
     newAllData.filter((item, i) => {
       if (item.i === parsedData.i) {
         indexes.push(i);
