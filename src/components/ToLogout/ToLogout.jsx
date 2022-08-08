26d463c0aac6138ab79422ff584e1f2ba6331526
Sito8943 <sito8943@gmail.com>
2022-08-07 18:30:01 -0700

    Tooltips

--- src/components/ToLogout/ToLogout.jsx
+++ src/components/ToLogout/ToLogout.jsx
@@ -5,10 +5,14 @@ import { useNavigate } from "react-router-dom";
 import KeyOffIcon from "@mui/icons-material/KeyOff";
 
 // @mui components
-import { Button } from "@mui/material";
+import { Tooltip, Button } from "@mui/material";
+
+// contexts
+import { useLanguage } from "../../context/LanguageProvider";
 
 const ToLogout = () => {
   const navigate = useNavigate();
+  const { languageState } = useLanguage();
 
   const toLogout = () => navigate("/auth/logout");
 
@@ -31,23 +35,25 @@ const ToLogout = () => {
   }, [onScroll]);
 
   return (
-    <Button
-      onClick={toLogout}
-      variant="contained"
-      sx={{
-        borderRadius: "100%",
-        position: "fixed",
-        right: 10,
-        bottom: 10,
-        padding: "5px",
-        minWidth: 0,
-        transition: "all 500ms ease",
-        transform: visible ? "scale(1)" : "scale(0)",
-        zIndex: visible ? 20 : -1,
-      }}
-    >
-      <KeyOffIcon />
-    </Button>
+    <Tooltip title={languageState.texts.Tooltips.ToLogout} placement="top">
+      <Button
+        onClick={toLogout}
+        variant="contained"
+        sx={{
+          borderRadius: "100%",
+          position: "fixed",
+          right: 10,
+          bottom: 10,
+          padding: "5px",
+          minWidth: 0,
+          transition: "all 500ms ease",
+          transform: visible ? "scale(1)" : "scale(0)",
+          zIndex: visible ? 20 : -1,
+        }}
+      >
+        <KeyOffIcon />
+      </Button>
+    </Tooltip>
   );
 };
 
